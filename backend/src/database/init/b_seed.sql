c7f46b080b4dc3a3515df6a13c6b8226db8ff7e4
Administrator <atomictranquility@gmail.com>
2025-06-26 08:50:00 +1000

    added new schema and some test data

--- backend/src/database/init/b_seed.sql
+++ backend/src/database/init/b_seed.sql
@@ -1,4 +1,9 @@
-INSERT INTO users (email, password) VALUES
-  ('alice@example.com', 'hashedpass123'),
-  ('bob@example.com', 'hashedpass456'),
-  ('carol@example.com', 'hashedpass789');
+INSERT INTO Account (email, phone, verified, pword) VALUES
+  ('alice@example.com', '0411212122', TRUE, 'hashedpass123'),
+  ('bob@example.com', '0411212122', TRUE, 'hashedpass456'),
+  ('carol@example.com', '0411212122', FALSE, 'hashedpass789');
+
+  INSERT INTO Wallet (balance, currency, Account) VALUES
+  (0, 'AUD', (SELECT aId FROM Account WHERE email = 'alice@example.com')),
+  (200, 'USD', (SELECT aId FROM Account WHERE email = 'bob@example.com')),
+  (10000, 'YEN', (SELECT aId FROM Account WHERE email = 'carol@example.com'));
