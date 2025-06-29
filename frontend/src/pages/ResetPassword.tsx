e0f315bda4290bab4b806367a019587025a5216d
Edwin <edwinni@outlook.com.au>
2025-06-29 16:51:35 +1000

    Updated routing and layout design

--- frontend/src/pages/ResetPassword.tsx
+++ frontend/src/pages/ResetPassword.tsx
@@ -1,6 +1,7 @@
 import { sendPasswordResetEmail } from "firebase/auth";
 import { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router";
+import logo from "../assets/logo.png";
 import { auth } from "../services/firebase";
 
 function ResetPassword() {
@@ -20,6 +21,7 @@ function ResetPassword() {
   return (
     <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+        <img alt="SENDIT Logo" src={logo} className="mx-auto h-20 w-auto" />
         <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">
           Reset your password
         </h2>
@@ -46,7 +48,7 @@ function ResetPassword() {
                 autoComplete="email"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
-                className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-black sm:text-sm/6"
               />
             </div>
           </div>
@@ -55,7 +57,7 @@ function ResetPassword() {
             <button
               type="button"
               onClick={handleReset}
-              className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 cursor-pointer"
+              className="flex w-full justify-center rounded-md bg-black px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-zinc-800 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black cursor-pointer"
             >
               Send Reset Email
             </button>
@@ -69,8 +71,8 @@ function ResetPassword() {
         <p className="mt-10 text-center text-sm/6 text-gray-500">
           Remember your password?{" "}
           <Link
-            to="/"
-            className="font-semibold text-indigo-600 hover:text-indigo-500"
+            to="/login"
+            className="font-semibold text-black hover:text-zinc-700"
           >
             Sign in
           </Link>
