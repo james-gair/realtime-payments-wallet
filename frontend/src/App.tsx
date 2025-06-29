6483fe307eaf7f16c0b17954eb4c0e6ddb6cb081
Edwin <edwinni@outlook.com.au>
2025-06-29 15:08:33 +1000

    Removed console log and changed login routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,7 +10,7 @@ import "./styles/App.css";
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<LoginPage />} />
+      <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<Register />} />
       <Route path="/dashboard" element={<Dashboard />} />
       <Route path="/resetPassword" element={<ResetPassword />} />
