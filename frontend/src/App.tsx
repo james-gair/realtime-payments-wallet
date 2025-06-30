c331bb2cd24b24afef4f65f975ef30610d0d4a80
Edwin <edwinni@outlook.com.au>
2025-06-30 13:37:04 +1000

    Made sure homepage opens login page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -12,6 +12,7 @@ import "./styles/App.css";
 function App() {
   return (
     <Routes>
+      <Route path="/" element={<LoginPage />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<Register />} />
       <Route path="/resetPassword" element={<ResetPassword />} />
