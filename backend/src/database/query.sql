fdd2dd58ae163a131d79b27fa9d5804e7d655d26
Jennifer-Uni <yujiayue921@outlook.com>
2025-07-13 12:04:13 +1000

    Modified query for manual testing.

--- backend/src/database/query.sql
+++ backend/src/database/query.sql
@@ -7,3 +7,7 @@ DELETE FROM account_identity WHERE account_id = 3;
 select * from account_identity;
 
 SELECT verified FROM Account WHERE firebase_id = 'ImwaeGpTjUhxe5GStIi1nwbYDj72';
+
+UPDATE Account
+SET verified = false
+WHERE account_id = 3;
