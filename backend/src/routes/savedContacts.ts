c231b4597c02d87a9ea4c5730c4fa2c425eabfa9
James Gair <jamesgair@student.unsw.edu.au>
2025-08-03 15:23:26 +1000

    added backend to add contacts

--- backend/src/routes/savedContacts.ts
+++ backend/src/routes/savedContacts.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { getSavedContacts, updateContactNickname } from "../handlers/savedContacts";
+import { addContact, getSavedContacts, updateContactNickname } from "../handlers/savedContacts";
 import { authenticateFirebaseToken } from "../middleware/auth";
 
 const router = Router();
 
 router.get("/saved-contacts", authenticateFirebaseToken as any, getSavedContacts);
 router.patch("/saved-contacts", authenticateFirebaseToken as any, updateContactNickname);
+router.post("/saved-contacts", authenticateFirebaseToken as any, addContact);
 
 export default router;
